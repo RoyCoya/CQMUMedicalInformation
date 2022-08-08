4898e01dbaa1307a0fe2c17603cea34772e7f1d7
Roy Coya <larysing@outlook.com>
2022-08-08 18:52:07 +0800

    [fixed] BoneAge ui and migration bugs

--- BoneAge/migrations/0011_preference.py
+++ BoneAge/migrations/0011_preference.py
@@ -8,7 +8,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
         ('BoneAge', '0010_dicomfile_age'),
     ]
 
