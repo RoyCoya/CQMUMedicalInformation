2beb6490a600210e43df2270e92a75dc848efcc8
RoyCoya <larysing@outlook.com>
2023-11-08 17:43:39 +0800

    [perf] Optimize BoneAge rectifier

--- BoneAge/management/commands/rectify_bone_age.py
+++ BoneAge/management/commands/rectify_bone_age.py
@@ -3,25 +3,32 @@ from django.core.management.base import BaseCommand
 from BoneAge.apis.Standard.Converter import GetBoneAge
 from BoneAge.models import BoneDetail, Task
 class Command(BaseCommand):
-    help = "2023-07-19 之前的系统内Task.bone_age都是默认值-1，没有经过计算，其内容都在前端。\n从07-18的commit后增加了计算，故以此脚本清洗之前尚计算的bone age。"
+    help = "根据当前的计算方式重新结算所有数据的骨龄"
 
     def handle(self, *args, **options):
         rectified_count = 0
         tasks = Task.objects.all()
         for i, task in enumerate(tasks):
             bones = BoneDetail.objects.filter(task=task)
-            self.stdout.write('scanning %d/%d' % (i+1, tasks.count()))
+            self.stdout.write('扫描 %d/%d, id: %s' % (i+1, tasks.count(), task.id))
             is_any_bone_error = sum(1 for bone in bones if bone.error != 0)
-            if not is_any_bone_error:
-                bone_age = GetBoneAge(
-                    standard=task.standard,
-                    sex = task.dcm_file.base_dcm.patient.sex,
-                    bones = bones
-                )
-                if not bone_age: continue
-                task.bone_age = bone_age
-                rectified_count += 1
-                task.save()
+            if is_any_bone_error:
+                print('存在错误的骨骼数据，跳过')
+                continue
+            bone_age = GetBoneAge(
+                standard=task.standard,
+                sex = task.dcm_file.base_dcm.patient.sex,
+                bones = bones
+            )
+            if not bone_age:
+                print('骨龄计算出错，跳过')
+                continue
+            origin_age = task.bone_age
+            task.bone_age = bone_age
+            print('原骨龄%s，更新为%s' % (str(origin_age), str(bone_age)))
+            rectified_count += 1
+            task.save()
+
         self.stdout.write(
             self.style.SUCCESS('错误骨龄校正成功，共修改 %s 条' % rectified_count )
         )
